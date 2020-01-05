87313bbee9f306c0cd34529885ceea4dae20f697
unknown <leedh2008@naver.com>
2020-01-05 21:57:53 +0900

    Algorithm

---
+++ BOJ14502_DH.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int arr[10][10];
+int ans = -1;
+int row, col;
+vector<pair<int, int> > v;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+int visited[10][10];
+
+void dfs (int y, int x){
+    int ret = 0;
+    visited[y][x] = 1;
+
+    if(arr[y][x] == 0)
+        arr[y][x] = 3;
+
+    for(int i=0; i<4; i++){
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if ( ny >= 0 && ny < row && nx >= 0 && nx < col && arr[ny][nx] == 0 && !visited[ny][nx])
+            dfs(ny, nx);
+    }
+}
+
+int count(){
+    int cnt = 0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(arr[i][j] == 0) cnt++;
+            if(arr[i][j] == 3) arr[i][j] = 0;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    cin >> row >> col;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            cin >> arr[i][j];
+            if(arr[i][j] == 0) v.push_back({i, j});  
+        }
+    }
+    int size = v.size();
+    for(int i=0; i<size; i++){
+        for(int j=i+1; j<size; j++){
+            for(int k=j+1; k<size; k++){
+                memset(visited, 0, sizeof(visited));
+                arr[v[i].first][v[i].second] = 1;
+                arr[v[j].first][v[j].second] = 1;
+                arr[v[k].first][v[k].second] = 1;
+                int ret = size;
+                for(int r=0; r<row; r++){
+                    for(int c=0; c<col; c++){
+                        if (arr[r][c] == 2 && !visited[r][c]) dfs(r, c);
+                    }
+                }
+                ans = max(ans, count());
+                arr[v[i].first][v[i].second] = 0;
+                arr[v[j].first][v[j].second] = 0;
+                arr[v[k].first][v[k].second] = 0;
+            }
+        }
+    }
+    cout << ans;
+    return 0;
+}
---
+++ BOJ7579_DH.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+struct my {
+	int b; // byte
+	int c; // cost
+};
+
+vector<my> v;
+int n, m;
+int dp[101][10001]; // dp[cur][cost] cost만큼 앱을 닫았을 때 최대 byte 값
+
+int go(int cur, int cost) {
+	
+	int& ret = dp[cur][cost];
+	if (ret != -1) return ret;
+
+	if (cur == n) return 0;
+
+	ret = go(cur + 1, cost); // 앱을 비활성화 하지 않음
+	if (cost - v[cur].c >= 0)
+		ret = max(ret, go(cur + 1, cost - v[cur].c) + v[cur].b);
+
+	return ret;
+}
+
+
+int main() {
+	cin >> n >> m;
+	v.resize(n); // first 사용중인 메모리, second 비활성화 소모비용
+	memset(dp, -1, sizeof(dp));
+	for (int i = 0; i < n; i++)
+		cin >> v[i].b;
+	for (int i = 0; i < n; i++)
+		cin >> v[i].c;
+	for (int i = 0; i <= 10000; i++) {
+		if (go(0, i) >= m) {
+			cout << i;
+			break;
+		}
+	}
+	return 0;
+}
