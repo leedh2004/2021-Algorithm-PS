f557e672ed3ae9129723debd392fe655b28823ac
JungJaeLee-JJ <tjems8637@gmail.com>
2020-01-19 18:41:49 +0900

    dp

---
+++ BOJ14501_JJ.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int n;
+int cost=0;
+pair<int,int> p[16];
+int f_day[16];
+int ck[16]={0};
+
+int dp[16]={0};
+
+void dpdp()
+{
+    for(int i=1;i<=n;i++)
+    {
+        if(f_day[i]==1) dp[1] = max(dp[0],p[i].second);
+    }
+    for(int i=2;i<=n;i++)
+    {
+        for(int j=i-1;j>=0;j--)
+        {
+            for(int k=j+1;k<=i;k++)
+            {
+                if(f_day[k]<=i)
+                {
+                    dp[i]=max(dp[i],dp[j]+p[k].second);
+                }
+                dp[i]=max(dp[i],dp[i-1]);
+            }
+        }
+        //cout<<dp[i]<<" \n";
+    }
+}
+
+
+int main()
+{
+    cin>>n;
+    for(int i=1;i<=n;i++) cin>>p[i].first>>p[i].second;
+    for(int i=1;i<=n;i++) f_day[i]=i+p[i].first-1;
+    dpdp();
+    cout<<dp[n];
+    return 0;
+}
