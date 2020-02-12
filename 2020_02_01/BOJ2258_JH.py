69f6dfce10e8b75669513caed0c1ce92f0238755
jihunJeong <jihun2729@gmail.com>
2020-02-12 15:49:11 +0900

    Solve 12 Feb 20

--- 2020_02_01/BOJ2258_JH.py
+++ 2020_02_01/BOJ2258_JH.py
@@ -1,20 +1,20 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().split())
-
+M = -1 * M
 meat = [(0, 0)]
 for _ in range(N):
 	w, p = map(int, sys.stdin.readline().split())
-	meat.append((w, p))
+	meat.append((-1 * w, p))
 	
 meat = sorted(meat, key=lambda x: (x[1], x[0]))
 sum, price = 0, 0
-for i in range(len(meat)):
+for i in range(1, len(meat)):
 	sum += meat[i][0]
 	price = price + meat[i][1] if meat[i][1] == meat[i - 1][1] else 0
-	if sum >= M:
+	if sum <= M:
 		print(meat[i][1] + price)
 		break
 
-if sum < M:
+if sum > M:
 	print(-1)
