53204adfc49fec32d07878679d19a412004038ae
jihunJeong <jihun2729@gmail.com>
2020-02-13 09:43:58 +0900

    Complete problem 13 Feb 20

--- 2020_02_01/BOJ1817_JH.py
+++ 2020_02_01/BOJ1817_JH.py
@@ -2,17 +2,18 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 if N == 0:
+	print("0")
 	sys.exit()
 book = list(map(int, sys.stdin.readline().split()))
 cnt, weight = 1, 0
 
-for i in range(len(book)):
+for i in range(len(book)- 1, -1, -1):
 	if weight + book[i] > M:
 		weight = book[i]
 		cnt += 1
 	elif weight + book[i] == M:
 		weight = 0
-		if i != len(book) - 1:
+		if i != 0:
 			cnt += 1
 	else :
 		weight += book[i]
