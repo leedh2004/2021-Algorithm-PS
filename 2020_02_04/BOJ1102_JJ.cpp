88e68c5a64c0b9a2997e7ade1a385bccde528ef8
JungJaeLee-JJ <tjems8637@gmail.com>
2020-03-03 13:56:35 +0900

    HI

--- 2020_02_04/BOJ1102_JJ.cpp
+++ 2020_02_04/BOJ1102_JJ.cpp
@@ -1,39 +1,76 @@
 #include <iostream>
-#include <queue>
 #include <string>
 #include <climits>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int n,p;
-int dp[1<<16];
-int cost[17][17];
-int now=0;
+vector<pair<int,pair<int,int> > > edge;
 string tmp;
 
-//몇개가 켜져있는가?
-int bit_count(int x)
-{
-    if(x==0) return 0;
-    return x%2+bit_count(x/2);
-}
 
 int main()
 {
+    int now=0;int ans=0;int total=0;int val;
     cin>>n;
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=n;j++) cin>>cost[i][j];
+        for(int j=1;j<=n;j++) 
+        {
+            cin>>val;
+            if(i==j) continue;
+            edge.push_back( {val,{i,j}} );
+        }
     }
+
+    //cost가 낮은 순으로 정렬
+    sort(edge.begin(),edge.end());
+
+    //현재 발전소들의 상태 입력 받음
     string tmp;cin>>tmp;tmp=" "+tmp;
     for(int i=1;i<=n;i++) 
     {
-        if(tmp[i]=='Y') now=now+1;
+        if(tmp[i]=='Y') 
+        {
+            now=now+1;
+            total++;
+        }
         now=now<<1;
     }
     now=now>>1;
-    printf("%d\n",now);
+    //printf("%d\n",now);
     cin>>p;
-    //cout<<solve();
+
+    //이미 충족된경우
+    if(total>=p)
+    {
+        printf("0");
+        return 0;
+    }
+ 
+    //남은 발전소 횟수만큼 돈다
+    for(int i=total;i<=p;i++)
+    {
+        //모든 간선들에 대하여 확인
+        for(int j=0;j<edge.size();j++)
+        {
+            int bit_mask1 = !(1<<(n-edge[j].second.first));
+            int bit_mask2 = 1<< (n-edge[j].second.second);
+
+            //현재 꺼져있는 발전소에서 시작하는 edge
+            if( (now|bit_mask1)==now) continue;
+
+            //현재 켜져있는 발전소로 도착하는 edge
+            if( (now&bit_mask2)==bit_mask2) continue;
+
+            now=now|(1<<(n-edge[j].second.second));
+            ans=ans+edge[j].first;
+            total++;
+            break;
+        }
+    }
+    printf("%d",(total==p)?ans:-1);
     return 0;
 }
