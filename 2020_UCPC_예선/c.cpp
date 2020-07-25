b6a12d3acd0595bd761ab3b64a20bdebdccc510c
이도현 <leedh2008@naver.com>
2020-07-25 14:33:54 +0900

    DH

---
+++ 2020_UCPC_예선/c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+typedef long long ll;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    return 0;
+}
